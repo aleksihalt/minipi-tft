24c6b0acda286569884b9b432b37d21920c6d8ff
Aleksi Halttunen <45580522+aleksihalt@users.noreply.github.com>
2024-03-09 23:03:40 +0200

    Update rgb_display_minipitfttest.py

--- rgb_display_minipitfttest.py
+++ rgb_display_minipitfttest.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries # SPDX-License-Identifier: MIT
-import digitalio import board
-from adafruit_rgb_display.rgb import color565 from adafruit_rgb_display import st7789
+import digitalio
+import board
+from adafruit_rgb_display.rgb import color565 
+from adafruit_rgb_display import st7789
 # Configuration for CS and DC pins for Raspberry Pi cs_pin = digitalio.DigitalInOut(board.CE0)
 dc_pin = digitalio.DigitalInOut(board.D25) reset_pin = None
 BAUDRATE = 64000000 # The pi can be very fast! # Create the ST7789 display:
