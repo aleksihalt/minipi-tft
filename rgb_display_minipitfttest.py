e51e551d2c7d710b29a2db3470afdffc16af691d
Aleksi Halttunen <45580522+aleksihalt@users.noreply.github.com>
2024-03-09 23:05:07 +0200

    Update rgb_display_minipitfttest.py

--- rgb_display_minipitfttest.py
+++ rgb_display_minipitfttest.py
@@ -4,15 +4,21 @@ import board
 from adafruit_rgb_display.rgb import color565 
 from adafruit_rgb_display import st7789
 # Configuration for CS and DC pins for Raspberry Pi cs_pin = digitalio.DigitalInOut(board.CE0)
-dc_pin = digitalio.DigitalInOut(board.D25) reset_pin = None
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
 BAUDRATE = 64000000 # The pi can be very fast! # Create the ST7789 display:
 display = st7789.ST7789(
 board.SPI(), cs=cs_pin, dc=dc_pin, rst=reset_pin, baudrate=BAUDRATE, width=135, height=240, x_offset=53, y_offset=40,
 )
-backlight = digitalio.DigitalInOut(board.D22) backlight.switch_to_output()
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
 backlight.value = True
-buttonA = digitalio.DigitalInOut(board.D23) buttonB = digitalio.DigitalInOut(board.D24) buttonA.switch_to_input() buttonB.switch_to_input()
+buttonA = digitalio.DigitalInOut(board.D23) 
+buttonB = digitalio.DigitalInOut(board.D24) 
+buttonA.switch_to_input() 
+buttonB.switch_to_input()
 # Main loop: while True:
-if buttonA.value and buttonB.value: backlight.value = False # turn off backlight
+if buttonA.value and buttonB.value:
+  backlight.value = False # turn off backlight
 else:
-backlight.value = True # turn on backlight
+  backlight.value = True # turn on backlight
